b77ac765a99b05aa3b220577d0ae32ba467d3b08
fer0m <antsin93@gmail.com>
2024-06-18 12:07:18 +0200

    9. Rename file, added core dirs

--- main_config.py
+++ main_config.py
@@ -5,5 +5,8 @@ CORE_DIR = BASE_DIR / 'core'
 
 FONT_FILE_PATH = CORE_DIR / 'fonts' / 'orange juice 2.0.ttf'
 ICONS_DIR = CORE_DIR / 'icons'
+AVATAR_DIR = CORE_DIR / 'avatar'
 
 DEBUG = True
+
+CORE_DIRS = [AVATAR_DIR]

22378508ca8dcfa9f1073621dfbdf5905114e5d5
fer0m <antsin93@gmail.com>
2024-06-18 12:07:56 +0200

    11. Added temp drawer

--- core/element_drawer.py
+++ core/element_drawer.py
@@ -1,47 +1,53 @@
+import os
 import sys
 from pathlib import Path
 
 import cairosvg
-from PIL import Image, ImageFilter, ImageDraw, ImageOps, ImageFont
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
 
 from core.canvas_base import CanvasBase
 from core.weather_getter import WeatherDataClass
+import main_config
+from core.wrap_tools import set_font_size
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-import config
-
 
 class ElementDrawer(CanvasBase):
+    current_zone = 'UTC +01:00'
+
     def __init__(self, canvas: Image, file_name: str):
         super().__init__()
         self.canvas: Image = canvas
+        self.draw: ImageDraw = ImageDraw.Draw(self.canvas)
         self.file_name = file_name
+        self.font = ImageFont.truetype(str(main_config.FONT_FILE_PATH))
 
     def draw_time(self) -> None:
         self.draw_time_on_canvas()
         self.draw_utc_zone()
         self.canvas.save(self.file_name)
 
+    @set_font_size(240)
     def draw_time_on_canvas(self):
-        draw = ImageDraw.Draw(self.canvas)
-        font = ImageFont.truetype(str(config.FONT_FILE_PATH), 240)
-
-        bbox = draw.textbbox((0, 0), self.formatted_current_time, font=font)
+        bbox = self.draw.textbbox((0, 0), self.formatted_current_time, font=self.font)
         width_font, height_font = bbox[2] - bbox[0], bbox[3] - bbox[1]
         coordinate = ((self.canvas.width - width_font) / 2, (self.canvas.height - height_font) / 2)
-        draw.text(coordinate, self.formatted_current_time, fill=(255, 255, 255), font=font)
+        self.draw.text(coordinate, self.formatted_current_time, fill=(255, 255, 255), font=self.font)
 
+    @set_font_size(60)
     def draw_utc_zone(self):
-        current_zone = 'UTC +01:00'
-
-        draw = ImageDraw.Draw(self.canvas)
-        font = ImageFont.truetype(str(config.FONT_FILE_PATH), 60)
-
-        bbox = draw.textbbox((0, 0), current_zone, font=font)
+        bbox = self.draw.textbbox((0, 0), self.current_zone, font=self.font)
         width_font, height_font = bbox[2] - bbox[0], bbox[3] - bbox[1]
         coordinate = ((self.canvas.width - width_font) / 2, 380)
-        draw.text(coordinate, current_zone, fill=(255, 255, 255), font=font)
+        self.draw.text(coordinate, self.current_zone, fill=(255, 255, 255), font=self.font)
+
+    @set_font_size(60)
+    def draw_weather_temperature(self):
+        bbox = self.draw.textbbox((0, 0), self.current_zone, font=self.font)
+        width_font, height_font = bbox[2] - bbox[0], bbox[3] - bbox[1]
+        coordinate = ((self.canvas.width - width_font) / 2, 380)
+        self.draw.text(coordinate, self.current_zone, fill=(255, 255, 255), font=self.font)
 
     def draw_weather(self, weather_data: WeatherDataClass):
         icon_name: str = weather_data.weather_icon_name
@@ -51,8 +57,13 @@ class ElementDrawer(CanvasBase):
 
     @staticmethod
     def _get_icon(icon_name: str) -> Image:
-        icon_svg_path = f"{str(config.ICONS_DIR)}/{icon_name}.svg"
-        icon_png_path = f"{str(config.ICONS_DIR)}/{icon_name}.png"
+        icon_svg_path = f"{str(main_config.ICONS_DIR)}/{icon_name}.svg"
+        icon_png_path = f"{str(main_config.ICONS_DIR)}/{icon_name}.png"
+        if not os.path.exists(icon_svg_path):
+            raise FileNotFoundError(f"SVG file not found: {icon_svg_path}")
+        if not os.path.exists(icon_svg_path):
+            raise FileNotFoundError(f"SVG file not found: {icon_png_path}")
+
         cairosvg.svg2png(url=icon_svg_path, write_to=icon_png_path, output_width=1000, output_height=1000)
         icon = Image.open(icon_png_path).resize((75, 75), Image.LANCZOS)
         icon = icon.convert("RGBA")
@@ -67,3 +78,9 @@ class ElementDrawer(CanvasBase):
         icon = icon.filter(ImageFilter.FIND_EDGES)
         icon.putdata(new_data)
         return icon
+
+    def draw_temperature(self, weather_data: WeatherDataClass):
+        icon_name: str = str(weather_data.temperature)
+        icon: Image = self._get_icon(icon_name)
+        self.canvas.paste(icon, (130, 50), icon)
+        self.canvas.save(self.file_name)

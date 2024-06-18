64c2ba9aea6dc93d809981437b31af235c761ca0
fer0m <antsin93@gmail.com>
2024-06-18 12:08:22 +0200

    12. Created adjust brightness func

--- core/colored_canvas_creator.py
+++ core/colored_canvas_creator.py
@@ -2,20 +2,10 @@ from PIL import Image, ImageDraw
 
 from core.canvas_base import CanvasBase
 
-TIME_COLORS_RATIO = {
-    "6:00-9:00": ((255, 255, 204), (255, 229, 204)),
-    "9:00-12:00": ((255, 229, 204), (204, 229, 255)),
-    "12:00-15:00": ((204, 229, 255), (204, 255, 255)),
-    "15:00-18:00": ((204, 255, 255), (204, 255, 204)),
-    "18:00-19:30": ((255, 204, 204), (204, 204, 255)),
-    "19:30-21:00": ((204, 204, 255), (153, 153, 255)),
-    "21:00-24:00": ((153, 153, 255), (102, 102, 204)),
-    "00:00-3:00": ((102, 102, 204), (51, 51, 153)),
-    "03:00-6:00": ((51, 51, 153), (255, 255, 204)),
-}
-
 
 class ColoredCanvasCreator(CanvasBase):
+    brightness_factor = 0.90
+
     @staticmethod
     def interpolate_color(color1, color2, factor):
         """Interpolate between two RGB colors by a given factor (0.0 - 1.0)"""
@@ -25,7 +15,7 @@ class ColoredCanvasCreator(CanvasBase):
     def get_gradient_colors(time_str: str):
         current_hour, current_minute = map(int, time_str.split(':'))
         current_time_in_minutes = current_hour * 60 + current_minute
-        for interval in TIME_COLORS_RATIO:
+        for interval in CanvasBase.TIME_COLORS_RATIO:
             start_time, end_time = interval.split('-')
             start_hour, start_minute = map(int, start_time.split(':'))
             end_hour, end_minute = map(int, end_time.split(':'))
@@ -34,7 +24,7 @@ class ColoredCanvasCreator(CanvasBase):
             end_time_in_minutes = end_hour * 60 + end_minute
 
             if start_time_in_minutes <= current_time_in_minutes < end_time_in_minutes:
-                return TIME_COLORS_RATIO[interval], start_hour, end_hour
+                return CanvasBase.TIME_COLORS_RATIO[interval], start_hour, end_hour
 
     def calculate_time_factor(self, start_hour, end_hour) -> float:
         start_seconds = start_hour * 3600
@@ -56,9 +46,27 @@ class ColoredCanvasCreator(CanvasBase):
 
         return base
 
+    @staticmethod
+    def adjust_brightness(img: Image, brightness_factor: float) -> Image:
+        """ Adjust the image brightness."""
+
+        pixels = img.load()
+        width, height = img.size
+
+        for x in range(width):
+            for y in range(height):
+                r, g, b = pixels[x, y]
+                new_r = max(0, min(255, int(r * brightness_factor)))
+                new_g = max(0, min(255, int(g * brightness_factor)))
+                new_b = max(0, min(255, int(b * brightness_factor)))
+                pixels[x, y] = (new_r, new_g, new_b)
+
+        return img
+
     def canvas_create(self):
         start_end_color, start_hour, end_hour = self.get_gradient_colors(self.formatted_current_time)
-        time_factor = self.calculate_time_factor(start_hour, end_hour)
+        time_factor: float = self.calculate_time_factor(start_hour, end_hour)
         img: Image = self.create_gradient_image(start_end_color, time_factor)
+        img: Image = self.adjust_brightness(img, self.brightness_factor)
         img.save(self.filename)
         return img
